0ab98e643de27ac8b47ebf251df6a85f95a7c2af
Johannes Carlsen <hannes.mcman@gmail.com>
2018-12-05 16:33:19 -0600

    add euclidean_distance lib function

---
+++ lib/euclidean_distance.cpp
@@ -0,0 +1,10 @@
+#include <cmath>
+using namespace std;
+
+float euclidean_distance(vector<float> x, vector<float> y) {
+  float sum = 0;
+  for (int i=0; i < x.size(); i++) {
+    sum += pow(x[i] - y[i], 2);
+  }
+  return sqrt(sum);
+}
