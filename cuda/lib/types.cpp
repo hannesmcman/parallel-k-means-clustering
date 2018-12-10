89ff31188a4932b3c74a2743f929db8ef420f526
Bidit Sharma <sharmabidit@gmail.com>
2018-12-10 00:11:31 -0600

    added c++11 stuff back

--- cuda/lib/types.cpp
+++ cuda/lib/types.cpp
@@ -4,6 +4,10 @@
 #include <string>
 using namespace std;
 
+typedef unordered_map< string, vector<float> > data_map;
+typedef vector<vector<float>> centroid_vector;
+typedef vector< vector<string> > cluster_vector;
+
 typedef struct college_dataset{
     int size;
     int dimensions;
